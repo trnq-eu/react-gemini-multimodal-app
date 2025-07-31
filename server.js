af921da1a4a34337068db3a28a78fe720135c242
trnq-eu <s.trinchero@gmail.com>
2025-07-31 14:35:04 +0200

    handles image upload and saves the image to disk

--- server.js
+++ server.js
@@ -9,12 +9,32 @@ app.use(express.json())
 
 require('dotenv').config()
 
+const { GoogleGenerativeAI } = require("@google/generative-ai")
+new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
+
+
 const fs = require('fs')
 
 const multer = require('multer')
 
-const { GoogleGenerativeAI } = require("@google/generative-ai")
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public')
+    },
+filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname)
+    }
+})
 
-new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
+const upload = multer({ storage:storage }).single('file')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if(err) {
+            return res.send(500).json(err)
+        }
+    })
+
+})
 
 app.listen(PORT, () => console.log("Listening to changes on PORT " + PORT))
--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { useState } from "react"
+
 
 const App = () => {
+  const [ image, setImage ] = useState(null)
+
+  const uploadImage = async (e) => {
+    
+    const formData = new FormData()
+    formData.append("file", e.target.files[0])
+    setImage(e.target.files[0])
+
+    
+
+    try {
+      const options = {
+      method: "POST",
+      body: formData
+    }
+      const response = await fetch("http://localhost:8000/upload", options)
+
+      const data = await response.json()
+
+      console.log(data)
+
+    } catch (err) {
+        console.error(err)
+    }
+
+  }
+
+  console.log(image)
+
   return (
     <div className="app">
-      
+      {image && <img src={URL.createObjectURL(image)} alt="Immagine caricata" />}
+      <label htmlFor="files">upload an image</label>
+      <input onChange= {uploadImage} id="files" accept="image/*" type="file" hidden/>
     </div>
   )
 }
